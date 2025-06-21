96f0a9f1146ca911eddb035b145443a61b80ee0a
Aysha <sumij19@gmail.com>
2025-06-21 20:25:07 +0530

    selenium grid changes

--- src/test/java/swaglab_test/BaseClass.java
+++ src/test/java/swaglab_test/BaseClass.java
@@ -3,13 +3,18 @@ package swaglab_test;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
@@ -23,23 +28,36 @@ public class BaseClass {
 	XSSFSheet sheet;
 	
 	@BeforeMethod
-	public void SetUp() {
-		
-		String browserName = System.getProperty("Browser");
-		
-		if(browserName.equalsIgnoreCase("firefox")) {
-			driver = new FirefoxDriver();
-		}else {
-			driver = new ChromeDriver();
+	
+	public void SetUp() throws MalformedURLException {
+			
+			String BrowserName = System.getProperty("Browser");
+			DesiredCapabilities cap = new DesiredCapabilities();
+			
+			if(BrowserName.equalsIgnoreCase("firefox")) {
+				driver = new FirefoxDriver();
+			}else if(BrowserName.equalsIgnoreCase("firefox_grid")) {
+				cap.setBrowserName("firefox");
+				cap.setPlatform(Platform.WIN11);
+				URL HubURL = new URL("http://localhost:4444");
+				driver = new RemoteWebDriver(HubURL,cap);
+				
+			}else if(BrowserName.equalsIgnoreCase("chrome_grid")) {
+				
+				cap.setBrowserName("chrome");
+				cap.setPlatform(Platform.WIN11);
+				URL HubURL = new URL("http://localhost:4444");
+				driver = new RemoteWebDriver(HubURL,cap);
+			
+			}else {
+				driver = new ChromeDriver();
+			}
+			
+			driver.get("https://www.saucedemo.com/");
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		}
 
-		
-		//driver = new ChromeDriver();
-		driver.get("https://www.saucedemo.com/");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-	}
-	
 	
 	@AfterMethod
 	public void TearDown() {
